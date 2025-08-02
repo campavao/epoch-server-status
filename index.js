bcf113c450cc7f0c81c1eff0fcbb26eeb134a39d
DESKTOP-8SRFDDM\cam95 <cam9548@gmail.com>
2025-08-02 08:20:53 -0500

    add second site

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const CHECK_INTERVAL = INTERVAL * 1000; // 60 seconds
 const STATUS_ENDPOINT =
   "https://project-epoch-tracker.onrender.com/api/status/realms";
 
+const STATUS_ENDPOINT_TWO =
+  "https://epoch-status.info/api/trpc/post.getStatus?batch=1&input=%7B%220%22%3A%7B%22json%22%3Anull%2C%22meta%22%3A%7B%22values%22%3A%5B%22undefined%22%5D%7D%7D%7D";
+
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -28,7 +31,8 @@ client.once("ready", () => {
   const readyCheck = async () => {
     try {
       const channel = await client.channels.fetch(CHANNEL_ID);
-      await channel.send("🟢 Bot is now ONLINE!");
+      // await channel.send("🟢 Bot is now ONLINE!");
+      console.log("Bot is now ONLINE!");
     } catch (err) {
       console.error(`Failed to access channel ${CHANNEL_ID}:`, err);
     }
@@ -38,54 +42,59 @@ client.once("ready", () => {
 
   setInterval(async () => {
     try {
-      const res = await axios.get(STATUS_ENDPOINT);
-      const status = res.data?.status;
       const channel = await client.channels.fetch(CHANNEL_ID);
-      const kezan = res.data?.realms?.find((realm) => realm.name === "Kezan");
-      const gurubashi = res.data?.realms?.find(
-        (realm) => realm.name === "Gurubashi"
-      );
 
-      const authServerOnline = res.data?.authServerStatus;
-      const kezanOnline = kezan?.worldServerOnline;
-      const gurubashiOnline = gurubashi?.worldServerOnline;
+      const {
+        authServerOnline: authServerOnlineOne,
+        kezanOnline: kezanOnlineOne,
+        gurubashiOnline: gurubashiOnlineOne,
+      } = await checkStatusSiteOne();
 
-      if (status === "online" && !lastPostedOnline && authServerOnline) {
+      const {
+        authServerOnline: authServerOnlineTwo,
+        kezanOnline: kezanOnlineTwo,
+        gurubashiOnline: gurubashiOnlineTwo,
+      } = await checkStatusSiteTwo();
+
+      const authServerOnline = authServerOnlineOne || authServerOnlineTwo;
+      const kezanOnline = kezanOnlineOne || kezanOnlineTwo;
+      const gurubashiOnline = gurubashiOnlineOne || gurubashiOnlineTwo;
+
+      if (authServerOnline && !lastPostedOnline) {
         await channel.send("🟢 Auth Server is now ONLINE!");
+        console.log("Auth Server is now ONLINE!");
         lastPostedOnline = true;
       }
 
-      if (status === "online" && !lastPostedKezanOnline && kezanOnline) {
+      if (kezanOnline && !lastPostedKezanOnline) {
         await channel.send("🟢 Kezan is now ONLINE!");
+        console.log("Kezan is now ONLINE!");
         lastPostedKezanOnline = true;
       }
 
-      if (
-        status === "online" &&
-        !lastPostedGurubashiOnline &&
-        gurubashiOnline
-      ) {
+      if (gurubashiOnline && !lastPostedGurubashiOnline) {
         await channel.send("🟢 Gurubashi is now ONLINE!");
+        console.log("Gurubashi is now ONLINE!");
         lastPostedGurubashiOnline = true;
       }
 
-      if (status !== "online") {
-        if (lastPostedOnline && !authServerOnline) {
-          await channel.send("🔴 Auth Server is now OFFLINE!");
-          lastPostedOnline = false;
-        }
-
-        if (lastPostedKezanOnline && !kezanOnline) {
-          await channel.send("🔴 Kezan is now OFFLINE!");
-          lastPostedKezanOnline = false;
-        }
-        if (lastPostedGurubashiOnline && !gurubashiOnline) {
-          await channel.send("🔴 Gurubashi is now OFFLINE!");
-          lastPostedGurubashiOnline = false;
-        }
+      if (lastPostedOnline && !authServerOnline) {
+        await channel.send("🔴 Auth Server is now OFFLINE!");
+        console.log("Auth Server is now OFFLINE!");
+        lastPostedOnline = false;
       }
 
-      console.log("Checked server status");
+      if (lastPostedKezanOnline && !kezanOnline) {
+        await channel.send("🔴 Kezan is now OFFLINE!");
+        console.log("Kezan is now OFFLINE!");
+        lastPostedKezanOnline = false;
+      }
+
+      if (lastPostedGurubashiOnline && !gurubashiOnline) {
+        await channel.send("🔴 Gurubashi is now OFFLINE!");
+        console.log("Gurubashi is now OFFLINE!");
+        lastPostedGurubashiOnline = false;
+      }
     } catch (err) {
       console.error("Error checking status:", err.message);
     }
@@ -94,7 +103,53 @@ client.once("ready", () => {
 
 client.login(DISCORD_BOT_TOKEN);
 
-// const example_data = {
+const checkStatusSiteOne = async () => {
+  try {
+    const res = await axios.get(STATUS_ENDPOINT);
+    const authServerOnline = res.data?.authServerStatus;
+    const kezanOnline = res.data?.realms?.find(
+      (realm) => realm.name === "Kezan"
+    )?.worldServerOnline;
+    const gurubashiOnline = res.data?.realms?.find(
+      (realm) => realm.name === "Gurubashi"
+    )?.worldServerOnline;
+
+    const result = {
+      authServerOnline,
+      kezanOnline,
+      gurubashiOnline,
+    };
+
+    console.log("Checked status for site one", result);
+
+    return result;
+  } catch (error) {
+    console.error("Error checking status for site one:", error.message);
+  }
+};
+
+const checkStatusSiteTwo = async () => {
+  try {
+    const res = await axios.get(STATUS_ENDPOINT_TWO);
+    const authServerOnline = res.data[0].result.data.json.auth;
+    const kezanOnline = res.data[0].result.data.json.world1;
+    const gurubashiOnline = res.data[0].result.data.json.world2;
+
+    const result = {
+      authServerOnline,
+      kezanOnline,
+      gurubashiOnline,
+    };
+
+    console.log("Checked status for site two", result);
+
+    return result;
+  } catch (error) {
+    console.error("Error checking status for site two:", error.message);
+  }
+};
+
+// const example_data_one = {
 //   status: "offline",
 //   realms: [
 //     {
@@ -123,4 +178,30 @@ client.login(DISCORD_BOT_TOKEN);
 //   authServerStatus: false,
 //   lastChecked: "2025-07-29T01:14:18.1677566Z",
 //   lastOnline: null,
+// };
+
+// const example_data_two = {
+//   0: {
+//     result: {
+//       data: {
+//         json: {
+//           auth: true,
+//           authLastTime: "2025-08-02T13:09:26.017Z",
+//           world1: false,
+//           world1LastTime: "2025-08-02T08:53:10.180Z",
+//           world2: false,
+//           world2LastTime: "2025-07-28T09:53:10.180Z",
+//           checkedAt: "2025-08-02T13:09:26.017Z",
+//         },
+//         meta: {
+//           values: {
+//             authLastTime: ["Date"],
+//             world1LastTime: ["Date"],
+//             world2LastTime: ["Date"],
+//             checkedAt: ["Date"],
+//           },
+//         },
+//       },
+//     },
+//   },
 // };
